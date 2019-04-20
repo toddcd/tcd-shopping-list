c4a5218148c95dbbec8036155f3c608d4d27f903
Todd Davis <tejastcd@gmail.com>
2019-04-20 15:27:51 -0400

    code formating

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
-    $('#js-shopping-list-form').submit(event =>{
+    $('#js-shopping-list-form').submit(event => {
         event.preventDefault();
 
         const item = $(this).find('input[name="shopping-list-entry"]').val();
-        const newItem = $('<li><span class="shopping-item">'+ item +'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+        const newItem = $('<li><span class="shopping-item">' + item + '</span>'
+            + '<div class="shopping-item-controls">'
+            + '<button class="shopping-item-toggle">'
+            + '<span class="button-label">check</span>'
+            + '</button><button class="shopping-item-delete">'
+            + '<span class="button-label">delete</span></button>'
+            + '</div>'
+            + '</li>');
 
         $(".shopping-list").append(newItem);
 
     });
 
     $('.shopping-list').on('click', 'li', function (event) {
-        if(event.target.innerText === "delete"){
+        if (event.target.innerText === "delete") {
             this.remove();
-        }else if(event.target.innerText === "check"){
+        } else if (event.target.innerText === "check") {
             $(this).find('span.shopping-item').toggleClass('shopping-item__checked');
         }
     });
